1c135b5030a555b3c9c64eae101cd38adcb921c2
NEER RAGHUWANSHI <neerraghuwanshi@NEERs-MacBook-Pro.local>
2020-04-29 16:47:12 +0530

    improved

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x)*cl&w_v19!(@=*@e)r5p7j-vhex8x^23q!=n^)!&(nn$m$7v'
+SECRET_KEY = config('SecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -49,8 +50,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 
     'User',
-    'payment',
-    'Bets',
 ]
 
 MIDDLEWARE = [
--- backend/urls.py
+++ backend/urls.py
@@ -21,12 +21,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-
     path('admin/', admin.site.urls),
-    
     path('user/', include('User.urls')),
-    path('payment/',  include('payment.urls')),
-    path('bets/', include('Bets.urls')),
     path('frontend/', TemplateView.as_view(template_name='index.html')),   
 ]
 
